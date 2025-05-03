aad7eecf631a0d449f681c8ede57900442f90578
Samarash Eugene <samarasha95@gmail.com>
2025-05-03 18:57:01 +0300

    task is ready

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+      <p className="Person__partner">
+        {person.isMarried &&
+          person.sex === 'm' &&
+          `${person.partnerName} is my wife`}
+        {person.isMarried &&
+          person.sex === 'f' &&
+          `${person.partnerName} is my husband`}
+        {!person.isMarried && 'I am not married'}
+      </p>
+    </section>
+  );
+};
