546bd37c2eeddd4679270c573f6538af5733ede9
Samarash Eugene <samarasha95@gmail.com>
2025-05-03 19:45:53 +0300

    Issue is handled

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import './Person.scss';
 
 export const Person = ({ person }) => {
+  let marriedText = '';
+
+  if (person.isMarried && person.sex === 'f') {
+    marriedText = `${person.partnerName} is my husband`;
+  } else if (person.isMarried && person.sex === 'm') {
+    marriedText = `${person.partnerName} is my wife`;
+  } else {
+    marriedText = 'I am not married';
+  }
+
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
       {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-      <p className="Person__partner">
-        {person.isMarried &&
-          person.sex === 'm' &&
-          `${person.partnerName} is my wife`}
-        {person.isMarried &&
-          person.sex === 'f' &&
-          `${person.partnerName} is my husband`}
-        {!person.isMarried && 'I am not married'}
-      </p>
+      <p className="Person__partner">{marriedText}</p>
     </section>
   );
 };
